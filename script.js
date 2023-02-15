61a86f93c60151e0b41411d023018b960458128c
Nicholas Preziosi <108760764+nicholaspreziosi@users.noreply.github.com>
2023-02-15 14:31:28 -0800

    Create playRound function using constant playerSelection

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function getComputerChoice () {
     let num = Math.floor((Math.random() * 3) + 1);
     if (num === 1) {
-        console.log('Rock');
+        return('rock');
     }
     else if (num === 2) {
-        console.log('Paper');
+        return('paper');
     }
     else {
-        console.log('Scissors');
+        return('scissors');
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return('Tie!');
+    }
+    else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
+        return('You lose! Paper beats Rock');
+    }
+    else {
+        return('You win! Rock beats Scissors');
+    }
+}
+
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
